14c75d50b98595c76055b3ea56140853c068f607
Salome Naskidashvili <send.to.salome@gmail.com>
2023-03-16 01:10:18 +0400

    third homework

--- 01_hello_world_2.py
+++
@@ -1,5 +0,0 @@
-#task 2
-#prints greeting with your name, *doesn't remove spaces inbetween
-#I can't do it without variable
-
-print("Hello, " + input("What is your name? ").strip().title() + ", Nice to meet you!")
---
+++ 03_playback.py/03_playback.py
@@ -0,0 +1,12 @@
+#get input from a user
+n = input("Write whatever you feel, this is a safe place\n")
+
+#turn sentence into a list
+n = n.split()
+
+#create string by connecting list's items using "..."
+n = "...".join(n)
+
+#print slower
+
+print(n)
