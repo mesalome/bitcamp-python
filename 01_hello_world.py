d446b598e809d6401b048a2ae347e7af2678ed83
Salome Naskidashvili <send.to.salome@gmail.com>
2023-03-16 00:23:50 +0400

    First Homework all 3 tasks

--- 01_hello_world.py
+++ 01_hello_world.py
@@ -1,1 +1,15 @@
-print("Hello World")
+#task 1
+#Asks and saves your name
+n = input("What is your name? ")
+
+# removes spaces from the begining and the end, every word is capitalized
+n = n.strip().title()
+
+# creates list of words
+n = n.split()
+
+#joins list items into a sentence and separates with space 
+n = " ".join(n)
+
+# #prints your name, removes spaces from the begining and the end, every word is capitalized
+print("Hello, " + n +", Nice to meet you!" )
---
+++ 01_hello_world_2.py
@@ -0,0 +1,5 @@
+#task 2
+#prints greeting with your name, *doesn't remove spaces inbetween
+#I can't do it without variable
+
+print("Hello, " + input("What is your name? ").strip().title() + ", Nice to meet you!")
