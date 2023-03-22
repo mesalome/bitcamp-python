0a6dcf8e182b78188206c0c2750e2e5501b270ca
Salome Naskidashvili <send.to.salome@gmail.com>
2023-03-22 20:05:05 +0400

    exercise 12

---
+++ 12_quotes/quotes_00.py
@@ -0,0 +1,4 @@
+print("What is the quote? \n"+
+    "These aren't the droids you're looking for.\n" + 
+    "Who said it? Obi-Wan Kenobi \n"+
+    "Obi-Wan Kenobi says,\"These aren't the droids you're looking for.")
---
+++ 12_quotes/quotes_challenge.py
@@ -0,0 +1,15 @@
+def main():
+    author = input("Please write author's name\n")
+    print(quotes[author])
+
+quotes = {
+    "Nelson Mandela" : "The greatest glory in living lies not in never falling\nbut in rising every time we fall.",
+    "Mahatma Gandhi" : "Learn as if you will live forever, live like you will die tomorrow.",
+    "Winston Churchill" : "Success is not final; failure is not fatal: It is the courage to continue that counts.",
+    "Dale Carnegie" : "The successful man will profit from his mistakes and try again in a different way.",
+    "Chris Grosser" : "Opportunities don't happen, you create them."
+
+
+}
+
+main()
