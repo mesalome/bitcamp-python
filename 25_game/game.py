bc5d377e97878b30a6ade6bb7b250c5f81e1f5bb
Salome Naskidashvili <send.to.salome@gmail.com>
2023-03-24 14:08:32 +0400

    exercise 25

---
+++ 25_game/game.py
@@ -0,0 +1,25 @@
+import random
+
+
+def main():
+
+    n = int(input("write a level : "))
+    myList = range(1, n)
+    guessingNumber = random.choice(myList)
+    while True:
+        try:
+            userInput = int(input("Guess: "))
+            if userInput > guessingNumber:
+                print("Too Large!")
+                continue
+            elif userInput < guessingNumber:
+                print("Too Littl!")
+                continue
+            else:
+                print("Just right! ")
+                break
+        except ValueError:
+            pass
+        else:
+            break
+main()
