3dffd68e29b332bf29a0d29bfe0df05b7ab26409
Salome Naskidashvili <send.to.salome@gmail.com>
2023-04-04 17:50:08 +0400

    exercise 37

---
+++ 37_watch/watch.py
@@ -0,0 +1,21 @@
+import re
+import sys
+
+
+def main():
+    print(parse(input("HTML: ")))
+
+
+def parse(s):
+    try: 
+        match = re.search(r"^.+src=\"https?://(?:www\.)?youtube\.com/embed/([^\"]+).+$", s) 
+        url = match.group(1)
+        return f"https://youtu.be/{url}"
+    except AttributeError:
+        return None
+    
+
+
+
+if __name__ == "__main__":
+    main()
