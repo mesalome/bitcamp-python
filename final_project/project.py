7f3106508065baee112896d8a40f21a540b0d4ab
Salome Naskidashvili <send.to.salome@gmail.com>
2023-04-21 02:45:19 +0400

    final project

--- final_project/project.py
+++ final_project/project.py
@@ -1,75 +1,79 @@
 import random
 import re
+from enum import IntEnum
     
+#this class makes code easier to read
+class Shoot(IntEnum):
+    Rock = 0
+    Paper = 1
+    Scissors = 2
+
+#after first game user will be asked if they want to keep playing or quit
+#if user writes something other than given 3 actions, they will be notified to fix it.
+#loop will be over if user press any other character than Y
 def main():
-    #after first game user will be asked if they want to keep playing or quit
-    #loop will be over if user press any other character than Y
     while True:
-        if match(user_input(), computer_input()) != None:
+        try:
+            # if match(user_input(), computer_input()) != None:
             print(match(user_input(), computer_input()))
-        question = input("If you want to keep playing press Y. To quit press any other character ")
-        if re.match("^[Y]{1}$", question, re.IGNORECASE):
-            continue
+        except TypeError:
+            print("Please write one of three possible actions")
+            pass
         else:
-            break
-        # if re.match("^[^y]$", question, re.IGNORECASE):
-        #     break
+            question = input("If you want to keep playing press Y. To quit press any other character ")
+            if re.match("^[Y]{1}$", question, re.IGNORECASE):
+                continue
+            else:
+                break
 
-# fanction changes user input into numbers
-# rock is 0
-# paper is 1
-# scissors is 2
-
+# fanction changes user input into numbers which now are called using Shoot class
+# rock is 0, same as Shoot.Rock
+# paper is 1 same as Shoot.Paper
+# scissors is 2 same as Shoot.Scissors
 def user_input():
     myinput = input("Rock, Paper or Scissors?: ").lower()
     if myinput == "rock":
-        return 0
+        return Shoot.Rock
     if myinput == "paper":
-        return 1
+        return Shoot.Paper
     if myinput == "scissors":
-        return 2
+        return Shoot.Scissors
+
 
 # computer's action is chosen randomly from list [0, 1, 2]
-
 def computer_input():
     return random.choice(range(3))
 
-#matching rules
-    # Rock beats Scissors
-    # Paper beats Rock
-    # Scissors beats Paper
 
-
+#basic matching rules
+# Rock beats Scissors
+# Paper beats Rock
+# Scissors beats Paper
 def match(user_input, computer_input):
-    draws = ["Rock", "Paper", "Scissors"]
+    shoots = ["Rock", "Paper", "Scissors"]
 
-#if user writes something other than given 3 actions, they will be notified to fix it.
-    try:
         #user's choice output on screen
-        print("User played: ", draws[user_input])
+    print("User played: ", shoots[user_input])
 
         #computer's choice output on screen
-        print("Computer played ", draws[computer_input])
-        if user_input == computer_input:
-            return "It's draw"
-        elif user_input == 0:
-            if  computer_input == 1:
-                return "You Lost!, Papper beats Rock"
-            elif computer_input == 2:
-                return "You Won!, Rock beats Scissors"
-        elif user_input == 1: 
-            if computer_input == 0:
-                return "You Won, Papper beats Rock"
-            elif computer_input == 2:
-                return "You Lost, Scissors beats Paper"
-        elif user_input == 2:
-            if computer_input == 1:
-                return "You Won, Scissors beats Paper"
-            elif computer_input == 0:
-                return "You Lost, Rock beats Scissors"
-    except TypeError:
-        print("Please write one of three possible actions")
-        pass
+    print("Computer played ", shoots[computer_input])
+    if user_input == computer_input:
+        return "It's a tie"
+    elif user_input == Shoot.Rock:
+        if  computer_input == Shoot.Paper:
+            return "You Lost!, Papper beats Rock"
+        elif computer_input == Shoot.Scissors:
+            return "You Won!, Rock beats Scissors"
+    elif user_input == Shoot.Paper: 
+        if computer_input == Shoot.Rock:
+            return "You Won, Papper beats Rock"
+        elif computer_input == Shoot.Scissors:
+            return "You Lost, Scissors beats Paper"
+    elif user_input == Shoot.Scissors:
+        if computer_input == Shoot.Paper:
+            return "You Won, Scissors beats Paper"
+        elif computer_input == Shoot.Rock:
+            return "You Lost, Rock beats Scissors"
     
     
 
